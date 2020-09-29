d5b6c09d7d43c8cb91acbf0cb04a2872feb53af9
Edson Alves <edson.alves@neki-it.com.br>
2020-09-29 08:28:46 -0300

    Exemplo com useEffect

--- src/components/LigadoDesligado.js
+++ src/components/LigadoDesligado.js
@@ -1,16 +1,25 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, Text, Button} from 'react-native';
 
 const LigadoDesligado = () => {
   const [isLigado, setIsLigado] = useState(false);
+  const [textoEstado, setTextoEstado] = useState('Off');
+  const [textoBotao, setTextoBotao] = useState('Ligar');
+
+  useEffect(() => {
+    if (isLigado) {
+      setTextoEstado('On');
+      setTextoBotao('Desligar');
+    } else {
+      setTextoEstado('Off');
+      setTextoBotao('Ligar');
+    }
+  }, [isLigado]);
 
   return (
     <View style={{margin: 12, alignItems: 'center'}}>
-      <Text> {isLigado ? 'On' : 'Off'} </Text>
-      <Button
-        title={isLigado ? 'Desligar' : 'Ligar'}
-        onPress={() => setIsLigado(!isLigado)}
-      />
+      <Text> {textoEstado} </Text>
+      <Button title={textoBotao} onPress={() => setIsLigado(!isLigado)} />
     </View>
   );
 };
